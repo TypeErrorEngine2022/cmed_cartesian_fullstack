a9d8592b12acc0792eb023c9e69633fb94db1c8e
Jacky <jackychen924@gmail.com>
2025-06-02 00:57:37 +0800

    feat: use database url for simpler connection

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -8,11 +8,7 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST || "localhost",
-  port: parseInt(process.env.DB_PORT || "5432"),
-  username: process.env.DB_USERNAME || "postgres",
-  password: process.env.DB_PASSWORD || "postgres",
-  database: process.env.DB_DATABASE || "cartesian",
+  url: process.env.DATABASE_URL,
   synchronize: process.env.NODE_ENV !== "production", // Only synchronize in development
   logging: process.env.NODE_ENV !== "production",
   ssl:
